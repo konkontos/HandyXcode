b9d5ec27cc8335a1d3793eddf39970f0b1031323
Konstantinos Kontos <kkon@handmadeapps.tech>
2016-11-22 11:37:36 +0200

    Initial Commit

---
+++ XcodeSourceEditorPlaceholder/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XcodeSourceEditorPlaceholder
+//
+//  Created by Konstantinos Kontos on 22/11/2016.
+//  Copyright © 2016 K. K. Handmade Apps Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
